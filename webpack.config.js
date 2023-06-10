3b06ef114a487ffe24e92bda2165214646b73876
audiomuse1 <andrewhartford@utexas.edu>
2023-06-10 16:22:17 -0500

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   mode: 'development',
   module: {
